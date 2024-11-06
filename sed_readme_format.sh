5c9a210149d7b93aeb07fdb6bcbbb3877ede674e
ayatakesi <ayanokoji.takesi@gmail.com>
2024-11-06 23:55:19 +0900

    rm nowrap

--- sed_readme_format.sh
+++ sed_readme_format.sh
@@ -2,12 +2,12 @@
 
 for ORIGINAL in README emacs-30/java/INSTALL emacs-30/admin/notes/java emacs-30/cross/ndk-build/README
 do
-    EDITABLE_FILE=${ORIGINAL}_nowrap;
+    EDITABLE_FILE=${ORIGINAL}_editable;
     PERL_OUT=$(mktemp);
     PO4A_OUT=$(mktemp);
     MSGCAT_OUT=$(mktemp);
     RM_FILES="$RM_FILES $PERL_OUT $PO4A_OUT $MSGCAT_OUT";
-    perl -l -000 -pe 's/^|$/\n/g if 2 != s/\n/ /g' -- $ORIGINAL >$PERL_OUT;
+    cat $ORIGINAL >$PERL_OUT;
     cp -p $PERL_OUT $EDITABLE_FILE;
     po4a-gettextize -o nobullets=1 -f text -m $EDITABLE_FILE -p $PO4A_OUT;
     msgcat --no-wrap $PO4A_OUT >$MSGCAT_OUT;
